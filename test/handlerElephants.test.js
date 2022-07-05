8786b2b8a7e31ea5f1046ce22f952b2aefe5d58c
Brenno-Lucas <brennolucas8@gmail.com>
2022-07-05 13:13:38 -0300

    Requisito 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,23 @@
+const { internet } = require('faker/lib/locales/pt_BR');
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('testa se a função retorna a quantidade de elefantes', () => {
+    expect(handlerElephants(handlerElephants('count')).toEqual(4));
+  });
+  it('testa se a função retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual([ 'Ilana', 'Orval', 'Bea', 'Jefferson' ]);
+  });
+  it('testa se a função retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants(handlerElephants('averageAge'))).toEqual(10.5);
+  });
+  it('testa se a função retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('testa se a função retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('popularity')).toStrictEqual(5);
+  });
+  it('testa se a função retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual([ 'Friday', 'Saturday', 'Sunday', 'Tuesday' ]);
+  });
+});
