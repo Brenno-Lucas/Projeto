026f28be494055b2429da59b338fa8f8f0e2ad4e
Brenno-Lucas <brennolucas8@gmail.com>
2022-07-06 13:22:12 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const numberChilds = entrants.filter((childs) => childs.age <= 12).length;
+  const numberAdults = entrants.filter((childs) => childs.age >= 18 && childs.age < 50).length;
+  const numberSeniors = entrants.filter((childs) => childs.age >= 50).length;
+
+  return { adult: numberAdults, child: numberChilds, senior: numberSeniors };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return data.prices.adult * adult + data.prices.child * child + data.prices.senior * senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
