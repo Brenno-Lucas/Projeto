4c1fce50059d0bd74ac873c7055cfb12593bce53
Brenno-Lucas <brennolucas8@gmail.com>
2022-07-07 17:59:46 -0300

    Requisitos 12/13 e 15/16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,43 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('testa se a função retorna todos horários caso o parametro esteja vazio', () => {
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(getOpeningHours()).toEqual(expected);
+  });
+  it('testa se a função retorna que o zoologico está fechado, caso o parametro seja monday', () => {
+    expect(getOpeningHours('monday', '09:00-PM')).toEqual('The zoo is closed');
+  });
+  it('testa se a função retorna que o zoologico está aberto, caso o parametro seja tuesday', () => {
+    expect(getOpeningHours('tuesday', '08:00-AM')).toEqual('The zoo is open');
+  });
+  it('testa se a função retorna que o zoologico está fechado, caso o parametro seja Wednesday às 09:00-PM', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
+  });
+  it('testa se a função retorna mensagem de erro, caso o parametro referente ao dia não corresponda a uma opção valida', () => {
+    expect(() => getOpeningHours('invalid', '09:00-AM')).toThrow('The day must be valid. Example: Monday');
+  });
+  it('testa se a função retorna mensagem de erro, caso a abreviatura referente ao horario não corresponda a uma opção valida', () => {
+    expect(() => getOpeningHours('Monday', '09:00-AX')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('testa se a função retorna mensagem de erro, caso as horas não correspondam a uma opção valida', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow(/^The hour should represent a number$/);
+  });
+  it('testa se a função retorna mensagem de erro, caso os minutos não correspondam a uma opção valida', () => {
+    expect(() => getOpeningHours('Saturday', '09:C0-AM')).toThrow(/^The minutes should represent a number$/);
+  });
+  it('testa se a função retorna mensagem de erro, quando não corresponder as regras ultilizadas para o horario', () => {
+    expect(() => getOpeningHours('Saturday', '13:00-AM')).toThrow(/^The hour must be between 0 and 12$/);
+  });
+  it('testa se a função retorna mensagem de erro, quando for passado 60 minutos como parametro', () => {
+    expect(() => getOpeningHours('tuesday', '08:60-AM')).toThrow('The minutes must be between 0 and 59');
+  });
+});
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -2,13 +2,13 @@ const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
   it('testa se a função retorna a quantidade de elefantes', () => {
-    expect(handlerElephants(handlerElephants('count')).toEqual(4));
+    expect(handlerElephants('count')).toEqual(4);
   });
   it('testa se a função retorna um array com a relação dos nomes de todos os elefantes', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
   });
   it('testa se a função retorna a média de idade dos elefantes', () => {
-    expect(handlerElephants(handlerElephants('averageAge'))).toEqual(10.5);
+    expect(handlerElephants('averageAge')).toEqual(10.5);
   });
   it('testa se a função retorna a localização dos elefantes dentro do Zoológico', () => {
     expect(handlerElephants('location')).toEqual('NW');
@@ -18,5 +18,14 @@ describe('Testes da função HandlerElephants', () => {
   });
   it('testa se a função retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
     expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('testa se a função retorna undefined, quando não se é passado argumentos para a função', () => {
+    expect(handlerElephants()).toEqual(undefined);
+  });
+  it('testa se a função retorna que o parâmetro é inválido, quando se é passado um objeto vazio de argumentos para a função', () => {
+    expect(handlerElephants({})).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+  it('testa se a função retorna null, quando é passada uma string que não contempla uma funcionalidade', () => {
+    expect(handlerElephants('opa')).toEqual(null);
   });
 });
