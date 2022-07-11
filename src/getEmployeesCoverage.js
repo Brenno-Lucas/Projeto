0902581f223407ecece6344a0cab4d19d818dd0b
Brenno-Lucas <brennolucas8@gmail.com>
2022-07-11 11:16:10 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+function createObject(param) {
+  if (param === undefined) {
+    const obj = employees.map((employee) => ({
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: employee.responsibleFor.map((e) => (
+        species.find((animal) => (animal.id === e)).name
+      )),
+      locations: employee.responsibleFor.map((e) => (
+        species.find((animal) => (animal.id === e)).location
+      )),
+    }));
+    return obj;
+  }
 }
+
+function getEmployeesCoverage(param) {
+  const test = param && createObject().find((e) => e.id === param.id
+  || e.fullName.split(' ')[0] === param.name
+  || e.fullName.split(' ')[1] === param.name);
+  if (test) {
+    return test;
+  }
+  if (param) {
+    throw new Error('Informações inválidas');
+  }
+  return createObject(param);
+}
+
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
